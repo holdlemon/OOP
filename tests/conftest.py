0626de2a56ba9334bb328cb1ca7a2cc924c820b8
daniil vazhov <d.vajov2010@yandex.ru>
2024-07-19 01:54:44 +0300

    Дополнен модуль src

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,34 +1,74 @@
 import pytest
 
-from src.product import Product
 from src.category import Category
+from src.product import Product
 
 
 @pytest.fixture
-def first_category():
-    return Category(
-        name="Телефоны",
-        description="Телефоны улучшают качество жизни",
-        products=[
-            Product("Samsung Galaxy C23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5),
-            Product("Iphone 15 Pro Max", "256GB, Серый цвет, 1000MP камера", 175000.0, 2)
-        ]
+def product1():
+    return Product(
+        name="Samsung Galaxy S23 Ultra", description="256GB, Серый цвет, 200MP камера", price=180000.0, quantity=5
     )
 
 
 @pytest.fixture
-def second_category():
+def product2():
+    return Product(name="Iphone 15", description="512GB, Gray space", price=210000.0, quantity=8)
+
+
+@pytest.fixture
+def product3():
+    return Product(name="Xiaomi Redmi Note 11", description="1024GB, Синий", price=31000.0, quantity=14)
+
+
+@pytest.fixture
+def product4():
+    return Product(name='55" QLED 4K', description="Фоновая подсветка", price=123000.0, quantity=7)
+
+
+@pytest.fixture
+def category1(product1, product2, product3):
+    return Category(
+        name="Смартфоны",
+        description="""Смартфоны, как средство не только коммуникации,
+но и получения дополнительных функций для удобства жизни""",
+        products=[product1, product2, product3],
+    )
+
+
+@pytest.fixture
+def category2(product4):
     return Category(
         name="Телевизоры",
-        description="Улучшают просмотр Ютуба в домашних условиях",
-        products=[
-            Product("Lenovo Ultra 4K", "Full HD, 4к формат", 200000.0, 3),
-            Product("LG", "Ultra Full HD, 8к формат", 300000.0, 1),
-            Product("Xiaomi", "Full HD, есть LED-подсветка", 180000.0, 6)
-        ]
+        description="""Современный телевизор, который позволяет наслаждаться просмотром, станет вашим другом и помощником""",
+        products=[product4],
     )
 
 
 @pytest.fixture
-def product():
-    return Product("Samsung Galaxy C23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
+def products_by_category_list_of_dict():
+    return [
+        {
+            "name": "Смартфоны",
+            "description": """Смартфоны, как средство не только коммуникации,
+            но и получение дополнительных функций для удобства жизни""",
+            "products": [
+                {
+                    "name": "Samsung Galaxy C23 Ultra",
+                    "description": "256GB, Серый цвет, 200MP камера",
+                    "price": 180000.0,
+                    "quantity": 5,
+                },
+                {"name": "Iphone 15", "description": "512GB, Gray space", "price": 210000.0, "quantity": 8},
+                {"name": "Xiaomi Redmi Note 11", "description": "1024GB, Синий", "price": 31000.0, "quantity": 14},
+            ],
+        },
+        {
+            "name": "Телевизоры",
+            "description": """Современный телевизор, который позволяет наслаждаться просмотром,
+            станет вашим другом и помощником""",
+            "products": [
+                {"name": '55" QLED 4K', "description": "Фоновая подсветка", "price": 123000.0, "quantity": 7}
+            ],
+        },
+    ]
